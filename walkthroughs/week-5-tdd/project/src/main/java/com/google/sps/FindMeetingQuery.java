84d1cc33c5c4887c43faf1e38c6081624f3514c9
Mauro_Guerrero <mauroguerrero20@gmail.com>
2020-06-15 21:25:38 +0000

    Query function pass all initial tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,112 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public final class FindMeetingQuery {
+
+  private boolean eventNoMandatoryAttendees(MeetingRequest request, Event event){
+    for (String attendee : event.getAttendees()){
+      if (request.getAttendees().contains(attendee)){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean nestedEventsCheck(TimeRange prevTimeRange, TimeRange currTimeRange){
+    if(prevTimeRange.start() < currTimeRange.start() && currTimeRange.end() < prevTimeRange.end()){
+      return true;
+    }
+    return false;
+  }
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    Collection<TimeRange> availableTimeRanges = new ArrayList<TimeRange>();
+
+    if (request.getDuration() > (long)TimeRange.WHOLE_DAY.duration()){
+      return availableTimeRanges;
+    }
+
+    if (events.isEmpty()){
+      availableTimeRanges.add(TimeRange.WHOLE_DAY);
+      return availableTimeRanges;
+    }
+
+    HashMap<TimeRange, Event> timeRangeEventMap = new HashMap<TimeRange, Event>();
+    ArrayList<TimeRange> timeRanges = new ArrayList<TimeRange>();
+
+    for (Event event : events){
+      timeRangeEventMap.put(event.getWhen(), event);
+      timeRanges.add(event.getWhen());
+    }
+    timeRanges.sort(TimeRange.ORDER_BY_START);
+
+
+    int avaliableStart = TimeRange.START_OF_DAY;
+    int avaliableEnd;
+
+    for (int index = 0; index < timeRanges.size(); index++){
+
+      Event currentEvent = timeRangeEventMap.get(timeRanges.get(index));
+
+      if (eventNoMandatoryAttendees(request, currentEvent)){
+        avaliableEnd = TimeRange.END_OF_DAY;
+        availableTimeRanges.add(TimeRange.fromStartEnd(avaliableStart, avaliableEnd, true));
+        continue;
+      }
+
+      avaliableEnd = timeRanges.get(index).start();
+      
+      // [##, ##)
+      TimeRange avaliableTimeExclusive = TimeRange.fromStartEnd(avaliableStart, avaliableEnd, false);
+      // [##, ## + 1)
+      TimeRange avaliableTimeInclusive = TimeRange.fromStartEnd(avaliableStart, avaliableEnd, true);
+
+      // Valid TimeRanges Check
+      if (!(avaliableTimeExclusive.overlaps(timeRanges.get(index)) || avaliableTimeExclusive.duration() < request.getDuration())){
+        availableTimeRanges.add(avaliableTimeExclusive);
+      }
+
+      if(!(avaliableTimeInclusive.overlaps(timeRanges.get(index)) || avaliableTimeInclusive.duration() < request.getDuration())){
+        availableTimeRanges.add(avaliableTimeInclusive);
+      }
+
+      // Nested Events Check
+      if (index != 0){
+
+        if (nestedEventsCheck(timeRanges.get(index - 1), timeRanges.get(index))){
+
+          if (index == timeRanges.size() - 1){
+            avaliableEnd = TimeRange.END_OF_DAY;
+            availableTimeRanges.add(TimeRange.fromStartEnd(avaliableStart, avaliableEnd, true));
+          }
+          else {
+            avaliableEnd = timeRanges.get(index + 1).start();
+            availableTimeRanges.add(TimeRange.fromStartEnd(avaliableStart, avaliableEnd, false));
+          }
+        continue;
+        }
+      }
+      
+      // Last Event on list
+      if (index == timeRanges.size() - 1){
+        
+        avaliableStart = timeRanges.get(index).end();
+        avaliableEnd = TimeRange.END_OF_DAY;
+
+        TimeRange lastTimeRange = TimeRange.fromStartEnd(avaliableStart, avaliableEnd, true);
+
+        if (!(lastTimeRange.duration() < request.getDuration())){        
+          availableTimeRanges.add(lastTimeRange);
+        }
+      }
+
+      avaliableStart = timeRanges.get(index).end();
+    }
+
+    return availableTimeRanges;
   }
 }
